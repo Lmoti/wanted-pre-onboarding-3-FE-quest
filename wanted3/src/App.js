dc158b63c4c301be1a79afb586fa102aef7038b0
Lmoti <tv13579@naver.com>
2023-02-26 20:38:09 +0900

    뼈대구조 구축

--- wanted3/src/App.js
+++ wanted3/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import PageA from "./pages/PageA";
+import PageB from "./pages/PageB";
+import PageC from "./pages/PageC";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <SideBar />
+      <Routes>
+        <Route path="/" element={<PageA />} />
+        <Route path="/pageB" element={<PageB />} />
+        <Route path="/pageC" element={<PageC />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
